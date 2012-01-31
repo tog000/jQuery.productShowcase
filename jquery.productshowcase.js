f94752c0a0a3f862160cc5ce8de101b17bab6ec8
Gabriel Trisca <tog000@gmail.com>
2012-01-31 11:12:36 -0600

    Hola

---
+++ jquery.productshowcase.js
@@ -0,0 +1,27 @@
+(function($) {
+
+    $.fn.productshowcase = function(options) {
+        var defaults = {
+			offsetx: 300
+        };
+
+        if (options) {
+			$.extend(defaults, options);
+		}
+
+        return this.each(function() {
+            
+        });
+
+    };
+
+	/*
+	// public functions definition
+	$.fn.pluginName.functionName = function(foo) {
+		return this;
+	};
+
+	// private functions definition
+	function foobar() {}
+	*/
+})(jQuery);
